8e2d92569175009097cbd46a782c7247c1b0ccac
Poile <poile@poile-VirtualBox.(none)>
2012-11-06 18:55:15 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '92dbf136453b2cf474ae66481ea629818ed73929d9e0f882a01583afd5e9345e08c59ccc33728e3b9cc4c1d1118fb3071493f7497c7050df3bdd6f19c55742f3'
